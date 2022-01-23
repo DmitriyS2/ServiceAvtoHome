1c1b06563ef51eb55f015af443679b7e4c21a871
Dmitriy <Home@mail.ru>
2022-01-23 10:18:02 +0300

    First

---
+++ ServiceAvtoHome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ServiceAvto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? user;
+            while (true)
+            {
+                Console.WriteLine("Введите Ваш статус (администратор или клиент): ");
+                user = Console.ReadLine();
+                if (user == "Администратор" || user == "администратор" || user == "Клиент" || user == "клиент") break;
+            }
+            Console.WriteLine($"Добрый день, {user}!");
+
+        }
+    }
+}

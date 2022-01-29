8ae3f43f8acf445092956fc0fec2350c1cdd7379
Dmitriy <d@mail.ru>
2022-01-29 20:20:21 +0300

    добавил 1й класс

---
+++ ServiceAvtoHome/Avto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAvtoHome
+{
+    internal class Avto
+    {
+        public string color;
+        public string type;
+        public int yearEdition;
+        public string Print() 
+        {
+            return color + " " + type + " " + yearEdition;
+        }
+        
+  
+  
+       
+
+
+    }
+}

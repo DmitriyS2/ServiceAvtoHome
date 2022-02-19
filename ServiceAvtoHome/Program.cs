e46ac1d637b02dcdfe548adb8432a36474752a94
Dmitriy <Home@mail.ru>
2022-02-19 13:21:10 +0300

    доб методы

--- ServiceAvtoHome/Program.cs
+++ ServiceAvtoHome/Program.cs
@@ -24,6 +24,7 @@ namespace ServiceAvtoHome
             {
                 newLineParametr = MenuString(phrase);
                 if (masLine.Contains(newLineParametr.ToUpper()) == true) return newLineParametr.ToUpper();
+                else Console.WriteLine(masPhrases[25]);
             }
         }
                                
@@ -37,13 +38,13 @@ namespace ServiceAvtoHome
         }
         
         public static void AddNewAvto()
-        {
-            size++;      
+        {             
             Console.Clear();
+            size++;
             cars.Add(new Avto { marka = NewLineParametr(masPhrases[0], masMarka), type = NewLineParametr(masPhrases[1], masType), color = NewLineParametr(masPhrases[3], masColor), power = NewNumberParametr(masPhrases[4], 100, 300), yearEdition = NewNumberParametr(masPhrases[2], 2000, 2021), cost = NewNumberParametr(masPhrases[5], 10000, 30000) });
             wheels.Add(new Wheel { radius = NewNumberParametr(masPhrases[6], 16, 21), typeDisk = NewLineParametr(masPhrases[7], masDisk), typeTyre = NewLineParametr(masPhrases[8], masTyre) });
             options.Add(new Option { conditioner = NewLineParametr(masPhrases[9], masYN), heat = NewLineParametr(masPhrases[10], masYN), navigation = NewLineParametr(masPhrases[11], masYN) });
-            Console.WriteLine("\nДобавлена новая машина в каталог");
+            Console.WriteLine(masPhrases[38]);
             PrintAvto(size-1);            
             Pause();
         }
@@ -75,16 +76,61 @@ namespace ServiceAvtoHome
             }            
             choice = NewNumberParametr(masPhrases[14], 1, 3);             
             Console.WriteLine(credits[0].CreditAnswer(choice) + masPhrases[17]);
+            Pause();
         }
 
-        public static void Pause(string hello="")
+        public static void Pause(string? hello= null)
         {
             Console.WriteLine(hello);
             Console.WriteLine(masPhrases[19]);
             Console.ReadLine();
             Console.Clear();
         }
-               
+
+        public static void FindAvto(int i, ref int tempIndex, ref int counterCustomerAvto)
+        {           
+            tempIndex = i + 1;
+            Console.WriteLine(cars[i].CarComposition(i + 1));
+            counterCustomerAvto++;
+            
+        }
+
+        public static void SearchCar (string phrase1, string phrase2, string phrase3, int border1, int border2)
+        {
+            while (true)
+            {
+                Console.WriteLine(phrase1);
+                borderA = NewNumberParametr(phrase2, border1, border2);
+                borderB = NewNumberParametr(phrase2, border1, border2);
+                if (borderA > borderB)
+                {
+                    Console.WriteLine(phrase3);
+                    continue;
+                }
+                break;
+            }
+            Console.Clear();            
+        }
+
+        public static void AddCost(int add, ref string oldN, string newN)
+        {           
+            oldN = newN;
+            cars[tempIndex - 1].cost += add;
+        }
+
+        public static void AddCost(int add, ref int oldN, int newN)
+        {            
+            oldN = newN;
+            cars[tempIndex - 1].cost += add;
+        }
+
+        public static void SearchMarkaColor(string phrase1, List<string>masN, ref string parametr)
+        {
+            searchParametr = NewLineParametr(phrase1, masN);
+            Console.Clear();
+            for (int i = 0; i < size; i++) if (parametr == searchParametr) FindAvto(i, ref tempIndex, ref counterCustomerAvto);
+        }
+
         static void Main(string[] args)
         {
             CreateCatalogAvto();
@@ -93,7 +139,7 @@ namespace ServiceAvtoHome
             {
                 Console.Clear();
                 choice = MenuInt (masPhrases[20]);               
-                if (choice==1)
+                if (choice==1) // admin
                 {
                     user = "admin";                   
                     Pause($"\nДобрый день, {user}!");                    
@@ -118,107 +164,49 @@ namespace ServiceAvtoHome
                         }
                     }
                 }
-                if (choice==2)
+                if (choice==2) // клиент
                 {
                     user = MenuString(masPhrases[22]);                                  
                     Pause($"\nДобрый день, {user}!");
                     while (true)
                     {
                         choice = MenuInt (masPhrases[23]);                        
-                        counterCustomerAvto = 0; // счетчик машин в выборе клиента
-                        addCost = 0;
-                        tempIndex = 0;
+                        counterCustomerAvto = 0; // счетчик машин в выборе клиента                        
+                        tempIndex = 0; // порядковый номер машины найденной клиентом в базе
                         if (choice == 1) // поиск по марке
                         {
                             searchMarka = NewLineParametr(masPhrases[0], masMarka);
                             Console.Clear();
-                            for (int i = 0; i < size; i++)
-                            {
-                                if (searchMarka == cars[i].marka)
-                                {
-                                    // tempIndex - порядковый номер машины найденной клиентом в базе
-                                    tempIndex = i+1;
-                                    Console.WriteLine(cars[i].CarComposition(i+1));
-                                    counterCustomerAvto++;
-                                }
-                            }                         
+                            for (int i = 0; i < size; i++) if (searchMarka == cars[i].marka) FindAvto(i, ref tempIndex, ref counterCustomerAvto);   
                         }
                         if (choice == 2) // поиск по году выпуска
                         {
-                            while (true)
+                            SearchCar(masPhrases[24], masPhrases[2], masPhrases[25], 2000, 2021);
+                            for (int i = borderA; i < borderB + 1; i++)
                             {
-                                Console.WriteLine(masPhrases[24]);
-                                yearEditionA = NewNumberParametr(masPhrases[2], 2000, 2021);
-                                yearEditionB = NewNumberParametr(masPhrases[2], 2000, 2021);
-                                if (yearEditionA > yearEditionB)
-                                {
-                                    Console.WriteLine(masPhrases[25]);
-                                    continue;
-                                }
-                                break;
-                            }
-                            Console.Clear();
-                            for (int i = yearEditionA; i < yearEditionB + 1; i++)
-                            {
-                                for (int j = 0; j < size; j++)
-                                {
-                                    if (cars[j].yearEdition == i)
-                                    {
-                                        tempIndex = j + 1;
-                                        Console.WriteLine(cars[j].CarComposition(j+1));
-                                        counterCustomerAvto++; // x - счетчик машин в выборе клиента
-                                    }
-                                }
+                                 for (int j = 0; j < size; j++) if (cars[j].yearEdition == i) FindAvto(j, ref tempIndex, ref counterCustomerAvto);  
                             }                           
                         }
                         if (choice == 3) // поиск по мощности
                         {
-                            while (true)
+                            SearchCar(masPhrases[24], masPhrases[4], masPhrases[25], 100, 300);                          
+                            for (int i = borderA; i < borderB + 1; i++)
                             {
-                                Console.WriteLine(masPhrases[25]);
-                                powerA = NewNumberParametr(masPhrases[4], 100, 300);
-                                powerB = NewNumberParametr(masPhrases[4], 100, 300);
-                                if (powerA > powerB)
-                                {
-                                    Console.WriteLine(masPhrases[25]);
-                                    continue;
-                                }
-                                break;
-                            }
-                            Console.Clear();
-                            for (int i = powerA; i < powerB + 1; i++)
-                            {
-                                for (int j = 0; j < size; j++)
-                                {
-                                    if (cars[j].power == i)
-                                    {
-                                        tempIndex = j + 1;
-                                        Console.WriteLine(cars[j].CarComposition(j+1));
-                                        counterCustomerAvto++; // x - счетчик машин в выборе клиента
-                                    }
-                                }
+                                for (int j = 0; j < size; j++) if (cars[j].power == i) FindAvto(j, ref tempIndex, ref counterCustomerAvto);
                             }                         
                         }
                         if (choice == 4) // поиск по цвету
                         {
                             searchColor = NewLineParametr(masPhrases[3], masColor);
                             Console.Clear();    
-                            for (int i = 0; i < size; i++)
-                            {
-                                if (cars[i].color == searchColor)
-                                {
-                                    tempIndex = i + 1;
-                                    Console.WriteLine(cars[i].CarComposition(i+1));
-                                    counterCustomerAvto++;
-                                }
-                            }                          
+                            for (int i = 0; i < size; i++) if (cars[i].color == searchColor) FindAvto(i, ref tempIndex, ref counterCustomerAvto);  
                         }
                         if(choice > 0 && choice<5)
                         {
                             if (counterCustomerAvto == 0) Console.WriteLine(masPhrases[26]);
                             else {                                
                                 // choiseYN - выбор да/нет
-                                choiseYN = MenuString(masPhrases[27]); ;
+                                choiseYN = NewLineParametr(masPhrases[27], masYN);
                                 if(choiseYN.ToUpper() == "ДА")
                                 {
                                     if (counterCustomerAvto > 1)
@@ -232,65 +220,39 @@ namespace ServiceAvtoHome
                                     Console.Clear();
                                     Console.WriteLine(masPhrases[29]);
                                     Console.WriteLine(wheels[tempIndex - 1].WheelComposition());                                    
-                                    choiseYN = MenuString(masPhrases[27]);
+                                    choiseYN = NewLineParametr(masPhrases[27], masYN);
                                     if (choiseYN.ToUpper() == "НЕТ")
                                     {
                                         Console.WriteLine(masPhrases[30]);
                                         newRadius = NewNumberParametr(masPhrases[6], 16, 21);
-                                        addCost += (newRadius - wheels[tempIndex-1].radius)*500; 
-                                        wheels[tempIndex-1].radius = newRadius;
-                                        cars[tempIndex - 1].cost += addCost;
+                                        AddCost(((newRadius - wheels[tempIndex - 1].radius) * 500), ref wheels[tempIndex - 1].radius, newRadius);                                        
                                         
                                         Console.WriteLine(masPhrases[31]);
                                         newTypeDisk = NewLineParametr(masPhrases[7], masDisk);
-                                        if (wheels[tempIndex - 1].typeDisk != newTypeDisk)
-                                        {
-                                        addCost += 1000; 
-                                        wheels[tempIndex - 1].typeDisk = newTypeDisk;
-                                        cars[tempIndex - 1].cost += addCost;
-                                        }
+                                        if (wheels[tempIndex - 1].typeDisk != newTypeDisk) AddCost(1000, ref wheels[tempIndex - 1].typeDisk, newTypeDisk);                                      
                                         
                                         Console.WriteLine(masPhrases[32]);
                                         newTypeTyre = NewLineParametr(masPhrases[8], masTyre);
-                                        if (wheels[tempIndex - 1].typeTyre != newTypeTyre)
-                                        {
-                                        addCost += 1000; 
-                                        wheels[tempIndex - 1].typeTyre = newTypeTyre;
-                                        cars[tempIndex - 1].cost += addCost;
-                                        }
+                                        if (wheels[tempIndex - 1].typeTyre != newTypeTyre) AddCost(1000, ref wheels[tempIndex - 1].typeTyre, newTypeTyre);                                        
                                        
                                     }
                                     Console.Clear();
                                     Console.WriteLine(masPhrases[33]);
                                     Console.WriteLine(options[tempIndex - 1].OptionComposition());
-                                    choiseYN = MenuString(masPhrases[27]);
+                                    choiseYN = NewLineParametr(masPhrases[27], masYN);
                                     if (choiseYN.ToUpper() == "НЕТ")
                                     { 
                                          Console.WriteLine(masPhrases[34]);
                                          newConditioner = NewLineParametr(masPhrases[9], masYN);
-                                         if (options[tempIndex - 1].conditioner != newConditioner)
-                                         addCost += 500; 
-                                         options[tempIndex - 1].conditioner = newConditioner;
-                                         cars[tempIndex - 1].cost += addCost;
+                                         if (options[tempIndex - 1].conditioner != newConditioner) AddCost(500, ref options[tempIndex - 1].conditioner, newConditioner);                                        
                                         
                                          Console.WriteLine(masPhrases[35]);
                                          newHeat = NewLineParametr(masPhrases[10], masYN);
-                                         if (options[tempIndex - 1].heat != newHeat)
-                                         {
-                                         addCost += 500; 
-                                         options[tempIndex - 1].heat = newHeat;
-                                         cars[tempIndex - 1].cost += addCost;
-                                         }
+                                         if (options[tempIndex - 1].heat != newHeat) AddCost(500, ref options[tempIndex - 1].heat, newHeat);                                        
                                         
                                          Console.WriteLine(masPhrases[36]);
                                          newNavigation = NewLineParametr(masPhrases[11], masYN);
-                                         if (options[tempIndex - 1].navigation != newNavigation)
-                                         {
-                                         addCost += 500; 
-                                         options[tempIndex - 1].navigation = newNavigation;
-                                         cars[tempIndex - 1].cost += addCost;
-                                         }
-                                        
+                                         if (options[tempIndex - 1].navigation != newNavigation) AddCost(500, ref options[tempIndex - 1].navigation, newNavigation);                                       
                                     }
                                     Console.Clear();
                                     Console.WriteLine(masPhrases[37]);
--- ServiceAvtoHome/SimpleData.cs
+++ ServiceAvtoHome/SimpleData.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServiceAvtoHome
+﻿namespace ServiceAvtoHome
 {
     internal static class SimpleData
     {
-        public static string? user, searchMarka, searchColor, newTypeDisk, newTypeTyre, newConditioner, newHeat, newNavigation, choiseYN, newLineParametr;
-        public static int choice, nomerAvto, counterCustomerAvto, yearEditionA, yearEditionB, powerA, powerB, tempIndex, newRadius, addCost, newNumberParametr;
+        public static string? user, searchMarka, searchColor, newTypeDisk, newTypeTyre, newConditioner, newHeat, newNavigation, choiseYN, newLineParametr, searchParametr;
+        public static int choice, nomerAvto, counterCustomerAvto, borderA, borderB, tempIndex, newRadius, newNumberParametr;
         public static int size = 10;
-  
+
         public static List<string> masMarka = new() { "FORD", "AUDI", "BMW", "OPEL", "MAZDA", "LADA", "MITSUBISHI", "NISSAN", "TOYOTA", "LEXUS", "FIAT", "RENAULT", "MERCEDES BENZ" };
         public static List<string> masType = new() { "СЕДАН", "ХЭТЧБЕК", "УНИВЕРСАЛ", "КУПЕ", "SUV" };
         public static List<string> masColor = new() { "БЕЛЫЙ", "ЧЕРНЫЙ", "СЕРЕБРИСТЫЙ", "КРАСНЫЙ", "СИНИЙ" };
@@ -21,44 +15,46 @@ namespace ServiceAvtoHome
         public static string[] masBank = { "СберБанк", "ВТБ", "АльфаБанк", "Тинькофф", "РайффайзенБанк", "АкБарсБанк" };
         public static string[] masPhrases =
         {
-            "\nВведите марку авто (FORD, AUDI, BMW, OPEL, MAZDA, LADA, MITSUBISHI, NISSAN, TOYOTA, LEXUS, FIAT, RENAULT, MERCEDES BENZ)",
+            "\nВведите марку авто (FORD, AUDI, BMW, OPEL, MAZDA, LADA, MITSUBISHI, NISSAN, TOYOTA, LEXUS, FIAT, RENAULT, MERCEDES BENZ)",//0
             "\nВведите тип кузова (Седан, Хэтчбек, Универсал, Купе, SUV)",
             "\nВведите год выпуска (в интервале 2000 - 2021)",
             "\nВведите цвет кузова (белый, черный, серебристый, красный, синий)",
             "\nВведите мощность (в интервале 100 - 300 л.с.)",
-            "\nВведите цену (в интервале 10000 - 30000$)",
+            "\nВведите цену (в интервале 10000 - 30000$)", //5
             "\nВведите радиус диска (в интервале 16 - 21)",
             "\nВведите тип диска (литой или штамп)",
             "\nВведите сезонность резины (зима или лето)",
             "\nВведите наличие кондиционера (да или нет)",
-            "\nВведите наличие зимнего пакета (да или нет)",
+            "\nВведите наличие зимнего пакета (да или нет)",//10
             "\nВведите наличие навигации (да или нет)",
             "\nВведите порядковый номер авто",
             "\nВведите новую цену ",
             "\nКакое предложение выбираете(1,2,3)?",
-            $"\n{user}, укажите как будете брать авто: \n1 - за наличные \n2 - в кредит",
-            $"\n{user}, для Вас есть три предложения по автокредиту:\n",
+            "\nУкажите как будете брать авто: \n1 - за наличные \n2 - в кредит",//15
+            "\nДля Вас есть три предложения по автокредиту:\n",
             "\nВтечение получаса мы подготовим Ваш автомобиль и выдадим его Вам. \nПоздравляем с покупкой!",
             "\nПроходите в кассу, расплачивайтесь.",
             "\nДля продолжения нажмите любую клавишу",
-            "Введите Ваш статус \n1 - администратор \n2 - клиент \n3 - СТОП ",
+            "Введите Ваш статус \n1 - администратор \n2 - клиент \n3 - СТОП ",//20
             "Что необходимо сделать: \n 1 - Вывести на экран все имеющиеся авто на складе \n 2 - добавить авто в каталог \n 3 - изменить цену авто \n 4 - выход",
             "\nКак к Вам обращаться?",
-            $"\n{user}, по какому параметру будем искать авто: \n 1 - марка авто \n 2 - год выпуска (2000-2021) \n 3 - мощность в л.с.(100-300) \n 4 - цвет \n 5 - выход",
+            "\nПо какому параметру будем искать авто: \n 1 - марка авто \n 2 - год выпуска (2000-2021) \n 3 - мощность в л.с.(100-300) \n 4 - цвет \n 5 - выход",
             "\nНужно ввести начало и конец интервала поиска. ",
-            "Неверное значение. Введите заново",
+            "\nНеверное значение. Введите заново", //25
             "\nК сожалению, у нас нет такой машины",
-            $"\n{user}, Вы останавливаетесь на этом выборе(да/нет)? ",
+            "\nВы останавливаетесь на этом выборе(да/нет)? ",
             "\nНайдено несколько машин. Введите номер авто в базе, который Вы планируете брать",
             "\nНа выбранной Вами машине установлены колеса:",
-            "\nСтоимость изменения радиуса на один размер - 500$",
+            "\nСтоимость изменения радиуса на один размер - 500$",//30
             "\nСтоимость изменения типа диска - 1000$ ",
             "\nСтоимость изменения типа резины - 1000$ ",
             "\nОпции на выбранной Вами машине:",
             "\nИзменение наличия кондиционера - 500$",
-            "\nИзменение наличия Зимнего пакета - 500$",
+            "\nИзменение наличия Зимнего пакета - 500$",//35
             "\nИзменение наличия навигации - 500$",
             "\nВаш итоговый автомобиль:",
+            "\nДобавлена новая машина в каталог",
+
         };
         public static List<Avto> cars = new();
         public static List<Wheel> wheels = new();
@@ -71,25 +67,25 @@ namespace ServiceAvtoHome
             for (int i = 0; i < size; i++)
             {
                 cars.Add(new Avto { marka = masMarka[rand.Next(0, 12)], color = masColor[rand.Next(0, 4)], type = masType[rand.Next(0, 4)], yearEdition = rand.Next(2000, 2021), power = rand.Next(2, 6) * 50, cost = rand.Next(10, 30) * 1000 });
-                wheels.Add(new Wheel { radius = 16, typeTyre = "ЛЕТО", typeDisk = "ШТАМП" });
-                options.Add(new Option { conditioner = "НЕТ", heat = "НЕТ", navigation = "НЕТ" });
+                wheels.Add(new Wheel { radius = 16, typeTyre = masTyre[1], typeDisk = masDisk[1] });
+                options.Add(new Option { conditioner = masYN[1], heat = masYN[1], navigation = masYN[1] });
             }
             return cars;
         }
 
         public static List<Credit> CreateCredit(int cost)
         {
-        for (int i=0; i<3;i++)
+            for (int i = 0; i < 3; i++)
             {
-                credits.Add(new Credit { totalCredit =0, bankName = masBank[rand.Next(0,5)], firstPayment = rand.Next(0,5), rate = rand.Next(5,10), sumCredit = 0,payment=0, periodCredit=rand.Next(1,3)});
-                credits[i].sumCredit = cost*(10-credits[i].firstPayment)/10;
+                credits.Add(new Credit { totalCredit = 0, bankName = masBank[rand.Next(0, 5)], firstPayment = rand.Next(0, 5), rate = rand.Next(5, 10), sumCredit = 0, payment = 0, periodCredit = rand.Next(1, 3) });
+                credits[i].sumCredit = cost * (10 - credits[i].firstPayment) / 10;
                 credits[i].totalCredit = credits[i].sumCredit;
                 for (int j = 1; j < credits[i].periodCredit; j++) credits[i].totalCredit = credits[i].totalCredit * (100 + credits[i].rate) / 100;
                 credits[i].payment = credits[i].totalCredit / credits[i].periodCredit / 12;
             }
-       
-        return credits;
+
+            return credits;
         }
-        
+
     }
 }
